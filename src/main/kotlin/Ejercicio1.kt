030c025ee2d0b150da1c2f28686bb0f9c4154108
Gabrieldb <72223819+Gdominguezborines@users.noreply.github.com>
2021-06-07 20:54:09 +0200

    Merge pull request #1
    * Ejercicio1 echo
    
    * Ejercicio1 echo con el main

---
+++ src/main/kotlin/Ejercicio1.kt
@@ -0,0 +1,5 @@
+class Ejercicio1(sizeArray: Int)  {
+
+         val array = Array(sizeArray){it * 6}
+
+    }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
-
-
+    for(numero in Ejercicio1(11).array) {
+        println(numero)
+    }
 
 
 
