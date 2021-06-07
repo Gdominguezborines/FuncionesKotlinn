926d76ac96cf956ada33545bd439820a45da99c7
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2021-06-07 21:01:19 +0200

    Ejercicio2 echo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
+import kotlin.random.Random
 fun main(args: Array<String>) {
 
 
-
+    val Ejercicio2 = Ejercicio2(10){
+        Random.nextInt(50,250)
+    }
+    Ejercicio2.array.forEach { println(it) }
 
 
 
